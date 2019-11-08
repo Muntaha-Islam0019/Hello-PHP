c10d6a81d7df02ce25670cf6010ab593c4f27f21
Muntaha Islam <muntaha.islam0019@gmail.com>
2019-11-08 18:27:18 +0600

    Minor changes over 'Hello'

--- Hello.php
+++ Hello.php
@@ -6,8 +6,15 @@
     Hello PHP!
 </h1>
 
+
 <?php
+
+    // In PHP, no keywords are case-sensitive. So, echo & EcHO are same!
     echo "Hello world!<br>";
+
+    /* Also, remember not to put comments outside the <php> tag.
+     * Or, it'd treated as a HTML syntax and will be printed.
+     */
 ?>
 
 </body>
