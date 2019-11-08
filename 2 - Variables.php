5a854cb890122e90913b5cb7866e9f467af9e0b5
Muntaha Islam <muntaha.islam0019@gmail.com>
2019-11-08 19:04:26 +0600

    Variables

--- 2 - Variables.php
+++ 2 - Variables.php
@@ -9,12 +9,21 @@
 <?php
 
     // Variables are declared by Dollar signs.
-    $a_variable = "A value.";
+    $a_variable = "A value";
 
     // Variables can be printed in 2 ways:
-    echo "Value of the variable: $a_variable.";
+    echo "Value of the variable: $a_variable.<br>";
     // Or
-    echo "Value of the variable: " . $a_variable . ".";
+    echo "Value of the variable: " . $a_variable . ".<br>";
+
+    $a_number = 19;
+    $another_number = 47;
+
+    echo $a_number + $another_number;
+
+    /* Though, PHP is a loosely typed language, type declarations were
+     * added in PHP 7.
+     */
 
 ?>
 
