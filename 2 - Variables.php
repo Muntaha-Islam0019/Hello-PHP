0017a0fb3c615056d2e094a130da327a6799bfff
Muntaha Islam <muntaha.islam0019@gmail.com>
2019-11-08 21:52:15 +0600

    Variables

--- 2 - Variables.php
+++ 2 - Variables.php
@@ -20,11 +20,62 @@
     $another_number = 47;
 
     echo $a_number + $another_number;
+    echo "<br>";
 
     /* Though, PHP is a loosely typed language, type declarations were
      * added in PHP 7.
      */
 
+    /* Variable scopes in PHP can be differentiated in 3 different ways:
+     * Local,
+     * Global,
+     * Static.
+     */
+
+    $x = 5;
+    $w = 10;
+    $v = 25;
+
+    function A_Func() {
+
+        $z = 15;
+
+        // This line is gonna return an error.
+        // echo "Variable x is: $x";
+    }
+
+    // Also, this is gonna return an error too.
+    // echo "Variable z is: $z";
+
+    /* So, by default we can't use local variables on global scope and
+     * global variables on local scope.
+     */
+
+    // This can be solved by using:
+    function Another_Func() {
+
+        global $w;
+        echo "Variable w is: $w.<br>";
+
+        // Or,
+        $new_var = $GLOBALS['v'];
+        echo "Variable v is $new_var.<br>";
+    }
+
+    Another_Func();
+
+    function Last_Func() {
+        static $y = 10;
+        echo "Variable y is: $y.<br>";
+        $y++;
+    }
+
+    Last_Func();
+    Last_Func();
+    Last_Func();
+    /* As, you can see, the value of y is changing after each call, this only
+     * happens when the function is static.
+     */
 ?>
 
 </body>
